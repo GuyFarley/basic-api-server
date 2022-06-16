bfb608b6ae8f26d2ab6a8ad2ee76d2fe9a89d660
Guy Farley <guy.e.farley@gmail.com>
2022-06-16 11:16:48 -0700

    Completed routes for whiskey

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
-# basic-api-server
+# LAB - Class 03 - Basic API Server
+
+## Author
+
+Guy Farley
+
+## Problem Domain
+
+The purpose of this lab is to perform CRUD Operations on a database and test for functionality.
+
+## Links and Resources
+
+<https://gf-basic-api-server.herokuapp.com/>
+
+<!-- CI/CD (GitHub Actions) -->
+
+## Setup
+
+Database URL: postgres://sjipcsuiiovocb:311a5ab72f07f45c1e524c40d7ae9411bb2200237f5564617e63b493b62c89bb@ec2-34-198-186-145.compute-1.amazonaws.com:5432/d33n319bajtmfe
+
+## How to initialize/run your application (where applicable)
+
+<!-- e.g. npm start -->
+
+## How to use your library (where applicable)
+
+## Features / Routes
+
+<!-- Feature One: Details of feature
+GET : /hello - specific route to hit -->
+
+## Tests
+
+Created tests using Jest and Supertest
+
+## UML
+
+<!-- Link to an image of the UML for your application and response to events -->
--- src/routes/whiskey.js
+++ src/routes/whiskey.js
@@ -5,6 +5,7 @@ const { WhiskeyModel } = require('../models');
 
 const router = express.Router();
 
+// post
 router.post('/whiskey', async (req, res, next) => {
   let whiskey = req.body;
   // console.log(req.body);
@@ -12,18 +13,38 @@ router.post('/whiskey', async (req, res, next) => {
   res.status(200).send(response);
 });
 
+// get all
 router.get('/whiskey', async (req, res, next) => {
-  let response = await WhiskeyModel.findAll();
+  let allWhiskeys = await WhiskeyModel.findAll();
   // console.log(response);
-  res.status(200).send(response);
+  res.status(200).send(allWhiskeys);
 });
 
+// get one
 router.get('/whiskey/:id', async (req, res, next) => {
-  let response = await WhiskeyModel.findAll({
-    where: { id: 1 },
-  });
-  console.log(response);
-  res.status(200).send(response);
+  let { id } = req.params;
+  let oneWhiskey = await WhiskeyModel.findOne({ where: { id } });
+  console.log(oneWhiskey);
+  res.status(200).send(oneWhiskey);
+});
+
+// put
+router.put('/whiskey/:id', async (req, res, next) => {
+  let { id } = req.params;
+
+  let updatedWhiskey = await WhiskeyModel.findOne({ where: { id } });
+  console.log(updatedWhiskey);
+  res.status(200).send(updatedWhiskey);
+});
+
+// delete
+router.delete('/whiskey/:id', async (req, res, next) => {
+  let { id } = req.params;
+  let deletedWhiskey = await WhiskeyModel.findOne({ where: { id } });
+
+  await WhiskeyModel.destroy({ where: { id } });
+  console.log(deletedWhiskey);
+  res.status(200).send(deletedWhiskey);
 });
 
 module.exports = router;
