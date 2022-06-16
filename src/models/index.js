4f338996080b1b360f88eda4512d8282a85e0f4c
Guy Farley <guy.e.farley@gmail.com>
2022-06-16 16:43:44 -0700

    changed config.json file

--- src/models/index.js
+++ src/models/index.js
@@ -3,13 +3,9 @@
 const { Sequelize, DataTypes } = require('sequelize');
 const whiskeySchema = require('./whiskey.schema');
 
-// const DATABASE_URL = process.env.NODE_ENV === 'test'
-//   ? 'sqlite::memory'
-//   : process.env.DATABASE_URL || 'postgres://localhost:5432/gf401-api-app';
-
 const DATABASE_URL = process.env.NODE_ENV === 'test'
   ? 'sqlite::memory'
-  : process.env.DATABASE_URL || 'postgres://sjipcsuiiovocb:311a5ab72f07f45c1e524c40d7ae9411bb2200237f5564617e63b493b62c89bb@ec2-34-198-186-145.compute-1.amazonaws.com:5432/d33n319bajtmfe';
+  : process.env.DATABASE_URL || 'postgres://localhost:5432/gf401-api-app';
 
 const sequelize = new Sequelize(DATABASE_URL, {
   dialectOptions: {
